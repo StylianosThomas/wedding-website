45ae577f8af981823752a3ebe76c7be7c2956d1e
Stylianos Thomas <stylianos.thomas@gmail.com>
2024-11-03 02:56:05 +0000

    Connected rsvp web app.js

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxZ2Dkfk8jwGsuqUkExFVrr4w0EGQl8xZNJN_zhcbxDJ2kT_VGmniUHLCo13SpA1sp20A/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
